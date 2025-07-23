774a0d73f4678a1a9a6deb1870b0d3d5dc8021b6
rhigby <ralphhigby@gmail.com>
2025-07-23 12:45:45 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import Lobby from './pages/Lobby'; // example
-import Login from './pages/Login'; // example
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MainRouter from './MainRouter';  // <-- Point to your new router setup
+import './index.css';
 
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/lobby" element={<Lobby />} />
-        <Route path="/login" element={<Login />} />
-        {/* add other routes */}
-      </Routes>
-    </Router>
-  );
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MainRouter />
+  </React.StrictMode>
+);
+
 
