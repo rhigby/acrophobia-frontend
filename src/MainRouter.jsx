9c0c7e960671502110421b156b49ca880abeb974
rhigby <ralphhigby@gmail.com>
2025-07-23 12:45:07 -0400

    Create MainRouter.jsx

---
+++ src/MainRouter.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import GameApp from './App';
+import Login from './pages/Login';
+
+export default function MainRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/play/*" element={<GameApp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
