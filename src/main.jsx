be23fe019b55aae6345d72ff99b83850a4b65f7e
rhigby <ralphhigby@gmail.com>
2025-07-23 15:15:14 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MainRouter from './MainRouter.jsx';  // <-- Point to your new router setup
+import MainRouter from './MainRouter';  // <-- Point to your new router setup
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
