e833038ce00033906da37e42c9cd605058dfcdda
rhigby <ralphhigby@gmail.com>
2025-07-23 13:44:30 -0400

    Update MainRouter.jsx

--- src/MainRouter.jsx
+++ src/MainRouter.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import landingpage from './pages/landingpage';
+import LandingPage from './pages/landingpage';
 import App from './App';
-//import Login from './pages/Login';
+// import Login from './pages/Login';
 
 export default function MainRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<landingpage />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/play/*" element={<App />} />
-        
+        {/* <Route path="/login" element={<Login />} /> */}
       </Routes>
     </Router>
   );
 }
+
