0bea60caba630bac733feb7aca15cc446b1d3033
rhigby <ralphhigby@gmail.com>
2025-07-23 16:03:45 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import fs from 'fs';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ✅ Add this
   build: {
     outDir: 'dist',
   },
-  publicDir: 'public', // makes sure _redirects is copied
 });
 
