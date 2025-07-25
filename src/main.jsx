60f5fcf48a02ee7ab6b82cf3e024e95cdf2d4115
rhigby <ralphhigby@gmail.com>
2025-07-25 10:50:19 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MainRouter from './MainRouter';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MainRouter />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
 
