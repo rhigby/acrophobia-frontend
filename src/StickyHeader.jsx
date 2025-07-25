3b39760a28fdd55ebe6de5e3c5fda4983adf988e
rhigby <ralphhigby@gmail.com>
2025-07-25 11:47:00 -0400

    Update StickyHeader.jsx

--- src/StickyHeader.jsx
+++ src/StickyHeader.jsx
@@ -1,1 +1,7 @@
-<div className="bg-red-500 text-white p-4 z-50 sticky top-0">Test Header</div>
+import { motion } from "framer-motion";
+
+export default function StickyHeader({ username, setProfileView, logout }) {
+  return (
+    <div className="bg-red-500 text-white p-4 z-50 sticky top-0">Test Header</div>
+  );
+}
