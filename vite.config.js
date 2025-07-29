3af1254c06386faa16a0212aff9044b2254e9c77
rhigby <ralphhigby@gmail.com>
2025-07-28 22:15:43 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ Add this
+  base: '/',
   build: {
     outDir: 'dist',
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // 👈 points to the inner /src/
+    },
+  },
 });
+
 
