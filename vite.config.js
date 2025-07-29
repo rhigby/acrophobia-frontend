490290d96b2cec6a91753efb62ab22d0c51378ad
rhigby <ralphhigby@gmail.com>
2025-07-28 22:27:21 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src/src'), // 👈 fix this
+      '@': path.resolve(__dirname, 'src'), // 👈 fix this
     },
   },
   base: '/',
