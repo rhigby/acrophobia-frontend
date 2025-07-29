45473ea32e75279f5dae1adfcf45b4cbc3c62447
rhigby <ralphhigby@gmail.com>
2025-07-28 22:00:32 -0400

    Update BotPromptModal.jsx

--- src/BotPromptModal.jsx
+++ src/BotPromptModal.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "../components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Slider } from "@/components/ui/slider";
 
